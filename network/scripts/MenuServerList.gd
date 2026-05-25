ae95b1e72bd2e7a0ac80356589ef9a4ae23e6512
Lamo <62069795+HonKLam@users.noreply.github.com>
2026-05-25 02:01:17 +0200

    feat: Add Serverlist

--- network/scripts/MenuServerList.gd
+++ network/scripts/MenuServerList.gd
@@ -8,8 +8,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func add_lobby(button: Button):
+func add_lobby(button: Button, lobby: int):
+	button.connect("pressed", Callable(self, "join_lobby").bind(lobby))
 	$CenterContainer/VBoxContainer.add_child(button)
+
+func join_lobby(lobby: int):
+	SceneLoader.goto_scene("res://network/testEnvironment/menu.tscn", false)
+	# Wait for menu to load, then trigger join
+	SceneLoader.scene_loading_finished.connect(func(_path):
+		SceneLoader.get_current_scene().join_and_go_to_next_scene(lobby)
+	, CONNECT_ONE_SHOT)
 
 func _on_back_button_pressed() -> void:
 	SceneLoader.goto_scene("res://network/testEnvironment/menu.tscn", false)
--- network/scripts/Networking.gd
+++ network/scripts/Networking.gd
@@ -189,17 +189,16 @@ func _on_lobby_match_list(these_lobbies: Array) -> void:
 	for this_lobby in these_lobbies:
 		# Pull lobby data from Steam, these are specific to our example
 		var lobby_name: String = Steam.getLobbyData(this_lobby, "name")
-		var lobby_mode: String = Steam.getLobbyData(this_lobby, "mode")
 
 		# Get the current number of members
 		var lobby_num_members: int = Steam.getNumLobbyMembers(this_lobby)
 
 		# Create a button for the lobby
 		var lobby_button: Button = Button.new()
-		lobby_button.set_text("Lobby %s: %s [%s] - %s Player(s)" % [this_lobby, lobby_name, lobby_mode, lobby_num_members])
+		lobby_button.set_text("Lobby %s: %s - %s Player(s)" % [this_lobby, lobby_name, lobby_num_members])
 		lobby_button.set_size(Vector2(800, 50))
 		lobby_button.set_name("lobby_%s" % this_lobby)
 		lobby_button.connect("pressed", Callable(self, "join_lobby").bind(this_lobby))
 		
-		server_list_menu.add_lobby(lobby_button)
+		server_list_menu.add_lobby(lobby_button, this_lobby)
 	SceneLoader.goto_preloaded_scene(server_list_menu, "res://network/testEnvironment/menuServerList.tscn")
