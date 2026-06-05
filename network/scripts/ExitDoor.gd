21ce150e41ff0e6c5ff60155aafb5ab477cec1a4
Finja Adam <165460369+finjaadam@users.noreply.github.com>
2026-06-05 16:40:42 +0200

    add spawning logic for exit doors

---
+++ network/scripts/ExitDoor.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+#func check_if_exit_should_open
+	#collider um Tür
+	#wenn alle Spieler:innen da drin und Schlüssel haben, geht Tür auf
+
+#func open_exit()
+	#Spieler:innen müssen ALLE jeweils mit Schlüssel GEMEINSAM vor einem Ausgang stehen
+	#dann erst geht der Ausgang auf
