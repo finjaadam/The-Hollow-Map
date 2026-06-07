14bf784cf5a2b42777cf54852053b3ac5d1bf96d
Finja Adam <165460369+finjaadam@users.noreply.github.com>
2026-06-07 15:39:59 +0200

    WIP: add key + key collectable

---
+++ autoload/TeamProperties.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var team_keys: int
+var team_lives: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+#signal keys_changed(new_amount)
+
+#signal lives_changed(new_amount)
+
+func collect_key() -> void:
+	print("Key aufgehoben")
---
+++ network/scripts/Key.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@onready var area: Area3D = $Area3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area.body_entered.connect(_on_body_entered)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body) -> void:
+	if body.is_in_group("player"):
+		TeamProperties.collect_key()
+		queue_free()
