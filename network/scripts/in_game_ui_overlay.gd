eccfad21cd4f05543f5d5d559022a1e0a1c9029a
Finja Adam <165460369+finjaadam@users.noreply.github.com>
2026-06-07 16:43:21 +0200

    bugfix crash when multiplayer

--- network/scripts/in_game_ui_overlay.gd
+++ network/scripts/in_game_ui_overlay.gd
@@ -3,11 +3,6 @@ extends CanvasLayer
 @onready var key_label = $KeyLabel
 
 func _ready() -> void:
-	# ui is only visible for the player, not for the others
-	if not is_multiplayer_authority():
-		$UI.visible = false
-		return
-	
 	key_label.text = "Schlüssel: %d" % TeamProperties.team_keys
 	TeamProperties.keys_changed.connect(_on_keys_changed)
 
