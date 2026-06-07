f631a69081e77d3657915de9626c6463fb326c01
Lamo <62069795+HonKLam@users.noreply.github.com>
2026-06-07 12:33:15 +0200

    fix: put send in string

--- autoload/Debug.gd
+++ autoload/Debug.gd
@@ -5,6 +5,6 @@ func print_bus_order() -> void:
 	for i in range(AudioServer.get_bus_count()):
 		var bus_name = AudioServer.get_bus_name(i)
 		var send = AudioServer.get_bus_send(i)
-		var send_str = send if send != "" else "none (master)"
+		var send_str = String(send) if send != "" else "none (master)"
 		print("[%d] %s  →  %s" % [i, bus_name, send_str])
 	print("=======================")
