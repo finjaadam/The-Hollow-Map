287115a3ff8f5020c279e53263e92ba9c8d09178
Emma Kirchner <emma.kirchner@develappers.de>
2026-06-07 17:16:58 +0200

    add rpc to jukebox

--- assets/jukebox/Jukebox.gd
+++ assets/jukebox/Jukebox.gd
@@ -2,10 +2,18 @@ extends Area3D
 
 @export var sounds: Array[AudioStream]
 
-@onready var audio: AudioStreamPlayer3D = $Audio
+@onready var audio: RaytracedAudioPlayer3D = $Audio
+
+func _ready():
+	audio.enabled.connect(BusManager.route_to_SFX_bus.bind(audio))
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
-		audio.stop()
-		audio.stream = sounds.pick_random()
-		audio.play()
+		var idx = randi() % sounds.size()
+		_play_jukebox.rpc(idx)
+
+@rpc("any_peer", "call_local", "unreliable")
+func _play_jukebox(sound_index: int):
+	audio.stop()
+	audio.stream = sounds[sound_index]
+	audio.play()
