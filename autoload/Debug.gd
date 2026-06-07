756ba9531389ba013dfd6ea09da870a73e7da8f8
Lamo <62069795+HonKLam@users.noreply.github.com>
2026-06-07 12:30:13 +0200

    rename name to bus_name

--- autoload/Debug.gd
+++ autoload/Debug.gd
@@ -3,8 +3,8 @@ extends Node
 func print_bus_order() -> void:
 	print("=== Audio Bus Order ===")
 	for i in range(AudioServer.get_bus_count()):
-		var name = AudioServer.get_bus_name(i)
+		var bus_name = AudioServer.get_bus_name(i)
 		var send = AudioServer.get_bus_send(i)
 		var send_str = send if send != "" else "none (master)"
-		print("[%d] %s  →  %s" % [i, name, send_str])
+		print("[%d] %s  →  %s" % [i, bus_name, send_str])
 	print("=======================")
