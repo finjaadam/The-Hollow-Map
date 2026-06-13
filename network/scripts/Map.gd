838695d2f923f2029b4cf0d156af5f26d5472a46
Lamo <62069795+HonKLam@users.noreply.github.com>
2026-06-13 10:14:37 +0200

    bugfix/sync-teamlives (#102)
    * wip: test reset player roles function
    
    * DEBUG: print player roles
    
    * wip: send signal to all players about player roles
    
    * fix: Clear Player Roles / erase player role when leaving
    
    * fix: Player Roles Sync using rpc
    
    * fix: debug
    
    * fix: send signal when player role sync is done
    
    * fix: missed signal on map load fix
    
    * fix: sync when someone changes roles
    
    * fix: debug
    
    * fix: Create GameManager --> Move TeamProperties into it + Adjust everything
    
    Split Game Logic and Networking Logic to make it more understandable
    
    * fix: Create Single Source of Truth Timer
    
    * fix: Reset TeamProperties when role is changed + private + public function renaming
    
    * chore: Cleanup
    
    ---------
    
    Co-authored-by: Finja Adam <165460369+finjaadam@users.noreply.github.com>
    Co-authored-by: Janek <104768529+JanekEngel@users.noreply.github.com>

--- network/scripts/Map.gd
+++ network/scripts/Map.gd
@@ -7,11 +7,8 @@ var spawn_exit_door_second_index: int
 
 var exit_door_scene = preload("res://network/testEnvironment/ExitDoor.tscn")
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	TeamProperties.reset()
-	
 	# get_children() gives back a const array => to remove a value from the array we need the second variable
 	var spawn_exit_door_points_dynamic = spawn_exit_door_points.get_children()
 	
@@ -23,9 +20,6 @@ func _ready() -> void:
 	spawn_exit_door_points_dynamic.remove_at(spawn_exit_door_first_index)
 	spawn_exit_door_second_index = randi() % spawn_exit_door_points_dynamic.size()
 	spawn_exit(spawn_exit_door_points_dynamic[spawn_exit_door_second_index])
-	
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -35,8 +29,3 @@ func spawn_exit(doorPosition: Marker3D) -> void:
 	var exit_door_scene_instance = exit_door_scene.instantiate()
 	self.add_child(exit_door_scene_instance)
 	exit_door_scene_instance.global_position = doorPosition.global_position
-	
-
-
-func _on_removes_lives_timer_timeout() -> void:
-	TeamProperties.remove_one_live()
