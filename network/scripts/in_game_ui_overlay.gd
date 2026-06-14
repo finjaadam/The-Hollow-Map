20866fbc025288577507f4c639801fd31f6b35d4
Finja Adam <165460369+finjaadam@users.noreply.github.com>
2026-06-14 20:44:40 +0200

    wip: color visibility

--- network/scripts/in_game_ui_overlay.gd
+++ network/scripts/in_game_ui_overlay.gd
@@ -9,15 +9,11 @@ extends CanvasLayer
 @onready var colored_keys = $colored_keys
 
 func _ready() -> void:
-	GameManager.keys_changed.connect(_on_keys_changed)
+	GameManager.keys_changed.connect(_set_key_visibility)
 	GameManager.state_updated.connect(_set_key_visibility)
 	
 	_set_key_visibility()
 	
-	# at the beginning no keys are collected
-	for key in colored_keys.get_children():
-		key.visible = false
-
 	if is_monster: 
 		return
 	
@@ -31,21 +27,16 @@ func _set_key_visibility() -> void:
 	if is_monster: 
 		return
 	
-	var limit = GameManager.get_player_count()
+	var limit = GameManager.team_keys
 
+	for key in colored_keys.get_children():
+		key.visible = key < limit
+		print("color visibility: ", key.visible)
 
-	for key in range(colored_keys.get_child_count()):
-		colored_keys.get_child(key).visible = key < limit
-	
 	limit = GameManager.get_player_count()
 	
 	for key in range(bw_keys.get_child_count()):
 		bw_keys.get_child(key).visible = key < limit
-
-func _on_keys_changed(amount: int) -> void:
-	if amount == 0: 
-		return
-	colored_keys.get_child(amount - 1).visible = true
 
 func _on_lives_changed(amount: int) -> void:
 	live_label.text = "Teamleben: %d" % amount
