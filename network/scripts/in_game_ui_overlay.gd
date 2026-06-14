129d51ff57c09d57d3ac9addd3ae126b8421772d
Finja Adam <165460369+finjaadam@users.noreply.github.com>
2026-06-14 19:41:37 +0200

    add 3 keys at hud

--- network/scripts/in_game_ui_overlay.gd
+++ network/scripts/in_game_ui_overlay.gd
@@ -5,9 +5,17 @@ extends CanvasLayer
 @onready var key_label = $KeyLabel
 @onready var live_label = $LiveLabel
 
+@onready var bw_keys = $bw_keys
+@onready var colored_keys = $colored_keys
+
 func _ready() -> void:
-	key_label.visible = !is_monster
+	bw_keys.visible = !is_monster
+	colored_keys.visible = !is_monster
+	
 	live_label.visible = !is_monster
+	
+	for key in colored_keys.get_children():
+		key.visible = false
 	
 	if is_monster: return
 	
@@ -18,7 +26,11 @@ func _ready() -> void:
 	GameManager.lives_changed.connect(_on_lives_changed)
 
 func _on_keys_changed(amount: int) -> void:
-	key_label.text = "Schlüssel: %d" % amount
+	
+	if amount == 0: 
+		return
+	print(colored_keys.get_child(amount).visible)
+	colored_keys.get_child(amount - 1).visible = true
 
 func _on_lives_changed(amount: int) -> void:
 	live_label.text = "Teamleben: %d" % amount
