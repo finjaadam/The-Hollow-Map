38a9144ee1b6056e13bea9373ae0759debb7353d
Finja Adam <165460369+finjaadam@users.noreply.github.com>
2026-06-14 20:23:41 +0200

    do not remove children, but change visibility

--- network/scripts/in_game_ui_overlay.gd
+++ network/scripts/in_game_ui_overlay.gd
@@ -10,17 +10,21 @@ extends CanvasLayer
 
 func _ready() -> void:
 	GameManager.keys_changed.connect(_on_keys_changed)
-	GameManager.state_updated.connect(_set_key_visibility_on_ready)
+	GameManager.state_updated.connect(_set_key_visibility)
 	
-	_set_key_visibility_on_ready()
+	_set_key_visibility()
 	
+	# at the beginning no keys are collected
+	for key in colored_keys.get_children():
+		key.visible = false
+
 	if is_monster: 
 		return
 	
 	live_label.text = "Teamleben: %d" % GameManager.team_lives
 	GameManager.lives_changed.connect(_on_lives_changed)
 
-func _set_key_visibility_on_ready() -> void:
+func _set_key_visibility() -> void:
 	bw_keys.visible = !is_monster
 	colored_keys.visible = !is_monster
 	
@@ -29,21 +33,14 @@ func _set_key_visibility_on_ready() -> void:
 	
 	var limit = GameManager.get_player_count()
 
-	for i in range(colored_keys.get_child_count() - 1, limit - 1, -1):
-		var child = colored_keys.get_child(i)
-		colored_keys.remove_child(child)
-		child.queue_free()
+
+	for key in range(colored_keys.get_child_count()):
+		colored_keys.get_child(key).visible = key < limit
 	
 	limit = GameManager.get_player_count()
-
-	for i in range(bw_keys.get_child_count() - 1, limit - 1, -1):
-		var child = (bw_keys.get_child(i))
-		bw_keys.remove_child(child)
-		child.queue_free()
 	
-	# at the beginning no keys are collected
-	for key in colored_keys.get_children():
-		key.visible = false
+	for key in range(bw_keys.get_child_count()):
+		bw_keys.get_child(key).visible = key < limit
 
 func _on_keys_changed(amount: int) -> void:
 	if amount == 0: 
