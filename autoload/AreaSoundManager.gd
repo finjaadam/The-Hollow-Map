0b68b8e700f58031b8339016fd74d13a8e41a5ec
Emma Kirchner <emma.kirchner@develappers.de>
2026-06-16 11:27:35 +0200

    add ambient sounds for stone, water and wood

--- autoload/AreaSoundManager.gd
+++ autoload/AreaSoundManager.gd
@@ -4,6 +4,31 @@ enum SurfaceType { STONE, GRAVEL, WATER, WOOD, SAND }
 
 const FOOTSTEP_LIBRARY: AudioLibrary = preload("res://audio/soundfx/map-areas/footstep_sounds.tres")
 
+var _default_player: RaytracedAudioPlayer3D
+
+func _ready():
+	_default_player = RaytracedAudioPlayer3D.new()
+	_default_player.name = "DefaultAmbientPlayer"
+	_default_player.stream = preload("res://audio/music/ambient/Stone.mp3")
+	_default_player.volume_db = -20.0
+	_default_player.unit_size = 1000.0
+	_default_player.max_distance = 0.0
+	_default_player.bus = &"Master"
+	_default_player.attenuation_model = RaytracedAudioPlayer3D.ATTENUATION_DISABLED
+	_default_player.autoplay = true
+	add_child(_default_player)
+
+
+func enter_zone():
+	var tween := create_tween()
+	tween.tween_property(_default_player, "volume_db", -80.0, 3.0).set_ease(Tween.EASE_IN_OUT)
+
+
+func exit_zone():
+	var tween := create_tween()
+	tween.tween_property(_default_player, "volume_db", -25.0, 3.0).set_ease(Tween.EASE_IN_OUT)
+
+
 static func surface_name(surface: SurfaceType) -> String:
 	return SurfaceType.keys()[surface].to_lower()
 
--- network/scripts/areaSounds/MapArea.gd
+++ network/scripts/areaSounds/MapArea.gd
@@ -1,6 +1,24 @@
 extends Area3D
 
 @export var surface_type: AreaSoundManager.SurfaceType = AreaSoundManager.SurfaceType.STONE
+@export var ambient_streams: Array[AudioStream]
+@export var ambient_volume_db: float = -10.0
+@export var ambient_pitch_scale: float = 1.0
+@export var ambient_unit_size: float = 10.0
+@export var ambient_max_distance: float = 15.0
+@export var ambient_position_offset: Vector3 = Vector3(0, 2, 0)
 
 func _ready():
 	add_to_group("footstep_region")
+
+	for i in ambient_streams.size():
+		var player = RaytracedAudioPlayer3D.new()
+		player.name = "AmbientPlayer_%d" % i
+		player.position = ambient_position_offset
+		player.stream = ambient_streams[i]
+		player.volume_db = ambient_volume_db
+		player.pitch_scale = ambient_pitch_scale
+		player.unit_size = ambient_unit_size
+		player.max_distance = ambient_max_distance
+		player.autoplay = true
+		add_child(player)
