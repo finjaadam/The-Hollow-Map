92edb069ad4bdf0856500a887d355932f30b4ef9
Lamo <62069795+HonKLam@users.noreply.github.com>
2026-06-21 17:13:26 +0200

    feat: add Ability to Monster-Scene + handle UI setup

--- network/monster/Monster.gd
+++ network/monster/Monster.gd
@@ -1,5 +1,7 @@
 extends General_Player
 
+@onready var ability_system: MonsterAbilitySystem = $AbilitySystem
+
 # do NOT create a _ready() function since it will overwrite the _ready from
 # General_Player --> Use _on_ready() instead
 func _on_ready() -> void:
@@ -9,3 +11,9 @@ func _on_ready() -> void:
 		$Model/BlobMonster.visible = false # you don't see model
 	else:
 		$Model/BlobMonster.visible = true # everyone else can see model
+
+# Override general setup and add the ability system to it
+func _setup_ui() -> void:
+	super._setup_ui()
+	var overlay = canvas as InGameUIOverlay
+	overlay.bind_ability_system(ability_system)
